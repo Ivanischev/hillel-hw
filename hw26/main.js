17c72586a3a3faa5efc946683e78a852d3382663
Иван Иванищев <ivanischev@MacBook-Pro-Ivan.local>
2020-11-16 01:08:46 +0200

    fixed hw26

--- hw26/main.js
+++ hw26/main.js
@@ -16,104 +16,51 @@ input.addEventListener('keyup', (event) => {
     }
 });
 
+
 function getPost() {
     let idInput = input.value;
-    
-    input.disabled = true;
 
     if(!isNaN(idInput)) {
         return showPost(idInput);
-        // await fetch(`${url}${postUrl}/${idInput}`)
-        //     .then(id => {return id.json();})
-        //     .then(id => {
-        //         return showPost(id);
-        //     })
-        // await fetch(`${url}${postUrl}/${idInput}/${commentUrl}`)
-        //     .then(com => {return com.json();})
-        //     .then(com => {
-        //         return showComment(com);
-        //     })
     }
 }
 
 
 async function showPost(id) {
     console.log("Start fetching...");
-    await fetch(`${url}${postUrl}/${id}`)
-    .then(id => {return id.json();})
-    .then(id => {
-        return htmlPostId(id);
-        console.log(id);
-    })
+    let postId = await fetch(`${url}${postUrl}/${id}`)
+        .then(id => {
+            return id.json();
+        })
+    showComment(id);
+    renderPost(postId);  
 }
 
-function htmlPostId(id) {
-    var postResult = "";
-    for (let i in id) {
-        if(id.hasOwnProperty(i)) {
-            postResult += i + "," + id[i];
+
+async function showComment(id) {
+    let commentId = await fetch(`${url}${commentUrl}/${id}`)
+        .then(id => {return id.json();})
+
+    renderPost(commentId);
+}
+
+
+function renderPost(postObj, commentId) {
+    let postText = "";
+    let postComment = "";
+
+    for (let i in postObj) {
+        if(postObj.hasOwnProperty(i)) {
+            postText += i + "," + postObj[i];
         }
     }
-    console.log(postResult);
-    return postResult;
-    
-    
+    for (let i in commentId) {
+        if(commentId.hasOwnProperty(i)) {
+            postComment += i + "," + commentId[i];
+        }
+    }
 
-    // let postData = id.toString;
-    // console.log(postData);
-    // let paragraph = document.createElement("p");
-    // paragraph.textContent += `${id.title}`;
-    // container.appendChild(paragraph);
-    // console.log(id);
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML += `${postText} <br> ${postComment}`;
+    container.appendChild(paragraph);
 }
-
-
-
-// fetch post & comments => new obj => add to postsHistory
-
-// async function main() {
-//     let id = input.value;
-    
-//     if(!isNaN(id)) {
-        
-//         input.disabled = true;
-
-//         await fetch(`${url}${postUrl}/${id}`)
-//             .then(data => {
-//                 return data.json();
-//             })
-//             .then(data => {
-//                 console.log(data);
-//                 return loadPost(data);
-//             })
-//             .then(({
-//                 title,
-//                 data
-//             }) => {
-//                 console.log(title);
-//                 input.disabled = false;
-//                 input.focus();
-//             })
-//             // .then(({
-//             //     comment,
-//             //     data,
-//             // }) => {
-//             //     container.append(comment);
-//             //     postsHistory.push(data);
-//             //     console.log(postsHistory);
-//             //     input.disabled = false;
-//             //     input.focus();
-//             //     debugger;
-//             // })
-            
-//     }
-
-// }
-
-// function loadPost(data) {
-//     return new Promise(function (resolve, reject) {
-//         Comment();
-//         Image();
-
-//     })
-// }
