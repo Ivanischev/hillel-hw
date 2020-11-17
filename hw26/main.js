f952f90a06f095466e035635934120aa05ab7637
Иван Иванищев <ivanischev@MacBook-Pro-Ivan.local>
2020-11-17 20:06:58 +0200

    added hw 26

--- hw26/main.js
+++ hw26/main.js
@@ -1,9 +1,9 @@
 const url = 'https://jsonplaceholder.typicode.com/';
 const postUrl = 'posts';
 const commentUrl = 'comments'
-let postsHistory = [];
 
-let container = document.querySelector('.comment-container');
+let comments = document.querySelector('#comments');
+
 let requestBtn = document.querySelector('#btnRequest');
 let input = document.querySelector('#textInput');
 
@@ -26,41 +26,45 @@ function getPost() {
 }
 
 
-async function showPost(id) {
-    console.log("Start fetching...");
-    let postId = await fetch(`${url}${postUrl}/${id}`)
-        .then(id => {
-            return id.json();
+function showPost(id) {
+
+    let commentId = fetch(`${url}${postUrl}/${id}/${commentUrl}`);
+    let postId = fetch(`${url}${postUrl}/${id}`);
+
+    
+    postId.then(request => {
+        return request.json();
+    })
+    .then(data => {
+        let postTitle = data.title;
+        let postBody = data.body;
+        let div = document.createElement('div');
+        let title = document.createElement('h3');
+        let description = document.createElement('p');
+        title.innerHTML = 'Title: ' + postTitle;
+        description.innerHTML = 'Body: ' + postBody;
+        div.append(title);
+        div.append(description);
+        posts.append(div);
+        
+        return commentId;
+    })
+    .then(request => {
+        return request.json();
+    })
+    .then(data => {
+        data.forEach(item => {
+            let commentName = item.name;
+            let commentBody = item.body;
+            let commentEmail = item.email;
+            let divComment = document.createElement('div');
+            let pComment = document.createElement('p');
+            pComment.innerHTML = `'comment data: '${commentName} <br> ${commentBody} <br> ${commentEmail}`;
+            divComment.append(pComment);
+            posts.append(divComment);   
+                            
+            input.value = "";
         })
-    showComment(id);
-    renderPost(postId);  
+    })
 }
 
-
-async function showComment(id) {
-    let commentId = await fetch(`${url}${commentUrl}/${id}`)
-        .then(id => {return id.json();})
-
-    renderPost(commentId);
-}
-
-
-function renderPost(postObj, commentId) {
-    let postText = "";
-    let postComment = "";
-
-    for (let i in postObj) {
-        if(postObj.hasOwnProperty(i)) {
-            postText += i + "," + postObj[i];
-        }
-    }
-    for (let i in commentId) {
-        if(commentId.hasOwnProperty(i)) {
-            postComment += i + "," + commentId[i];
-        }
-    }
-
-    let paragraph = document.createElement("p");
-    paragraph.innerHTML += `${postText} <br> ${postComment}`;
-    container.appendChild(paragraph);
-}
